20ea564fe70dfebb8bde6f6d8b5501e46a68410e
dr-fluff <oliver@Olivers-MacBook-Pro-2.local>
2016-09-26 10:04:18 +0200

    update sudoko p1
    updating sudoko

--- Labbar/Labb 3 Sudoku/Sudoku.hs
+++ Labbar/Labb 3 Sudoku/Sudoku.hs
@@ -37,9 +37,9 @@ isSudoku sud = and [numRows sud, numElem sud, valueElem sud]
   where
     numRows   sud = length (rows sud) == 9
     numElem   sud = and [length r == 9| r <- rows sud]
-    valueElem sud = and [and [x == Nothing || (x > Just 0 && x < Just 10) 
+    valueElem sud = and [and [x == Nothing || (x > Just 0 && x < Just 10)
                                                | x <- r] | r <- rows sud]
-     
+
 -- isSolved sud checks if sud is already solved, i.e. there are no blanks
 isSolved :: Sudoku -> Bool
 isSolved sud = and [Nothing `notElem` r | r <- rows sud]
@@ -49,20 +49,20 @@ isSolved sud = and [Nothing `notElem` r | r <- rows sud]
 -- printSudoku sud prints a representation of the sudoku sud on the screen
 printSudoku :: Sudoku -> IO ()
 printSudoku sud = putStr $ unlines [[printElem e | e <- r] | r <- rows sud]
-  where 
+  where
     printElem e | e == Nothing = '.'
                 | otherwise    = intToDigit(fromJust e)
 
--- readSudoku file reads from the file, and either delivers it, or stops
--- if the file did not contain a sudoku
+-- readSudoku file reads from the file, and either delivers it,
+-- or stops if the file did not contain a sudoku
 readSudoku :: FilePath -> IO Sudoku
 readSudoku file = do
     s <- readFile file
     if (isSudoku(symCon s)) then return (symCon s)
             else error ("Exception: Not a Sudoku!")
- where -- symCon takes the string of  '.' and numbers and returns a suduko 
+ where -- symCon takes the string of  '.' and numbers and returns a suduko
     symCon s = Sudoku [[symCon' e | e <- r] | r <- lines s]
-         where 
+         where
             symCon' e | e == '.'  = Nothing
                       | otherwise = Just (digitToInt e)
 
@@ -70,7 +70,8 @@ readSudoku file = do
 
 -- cell generates an arbitrary cell in a Sudoku
 cell :: Gen (Maybe Int)
-cell = undefined
+cell = frequency [(9, return Nothing), (1, do n <- choose (1,9)
+                                              return (Just n))]
 
 -- an instance for generating Arbitrary Sudokus
 instance Arbitrary Sudoku where
@@ -78,6 +79,9 @@ instance Arbitrary Sudoku where
     do rows <- sequence [ sequence [ cell | j <- [1..9] ] | i <- [1..9] ]
        return (Sudoku rows)
 
+-- test that generated sudukos are ture
+prop_Sudoku :: Sudoku -> Bool
+prop_Sudoku sud = isSudoku sud
 -------------------------------------------------------------------------
 
 solve :: Sudoku -> Maybe Sudoku
@@ -90,4 +94,3 @@ solve s | undefined = Nothing  -- There's a violation in s
 
 pickASolution :: [Maybe Sudoku] -> Maybe Sudoku
 pickASolution suds = undefined
-
