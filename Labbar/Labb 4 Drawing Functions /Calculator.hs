bf52245d0ebb1431bf0f81441b4c90134b3f73a4
dr-fluff <oliver@Olivers-MacBook-Pro-2.local>
2016-10-03 11:20:15 +0200

    yo

--- Labbar/Labb 4 Drawing Functions /Calculator.hs
+++ Labbar/Labb 4 Drawing Functions /Calculator.hs
@@ -1,4 +1,16 @@
+module Calculator where
+
+import Expr
+
 -- | -----------------Part II-----------------------------------------|--
 -- Assigment F
 -------------------------------------------------------------------------
 --
+points :: Expr -> Double -> (Int,Int) -> [Point]
+points = undefined
+
+-- Assigment G
+-------------------------------------------------------------------------
+--
+readAndDraw :: Elem -> Canvas -> IO ()
+readAndDraw = undefined
