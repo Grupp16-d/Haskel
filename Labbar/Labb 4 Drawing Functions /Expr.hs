780adbc9dd7fa48aa0674e574af031887e874825
dr-fluff <oliver@Olivers-MacBook-Pro-2.local>
2016-10-03 10:42:30 +0200

    New labb 4

---
+++ Labbar/Labb 4 Drawing Functions /Expr.hs
@@ -0,0 +1,42 @@
+module Expr where
+
+import Test.QuickCheck
+import Data.List
+import Data.List.Split
+
+-- | ------------------------- Part I --------------------------------|--
+-- Assignment A
+-------------------------------------------------------------------------
+--
+
+
+-- Assignment B
+-------------------------------------------------------------------------
+--
+showExpr :: Expr -> String
+showExpr = undefined
+
+
+--Assigment C
+-------------------------------------------------------------------------
+--
+eval :: Expr -> Double -> Double
+eval = undefined
+
+--Assigment D
+-------------------------------------------------------------------------
+--
+readExpr :: String -> Maybe Expr
+readExpr = undefined
+
+--Assigment E
+-------------------------------------------------------------------------
+--
+prop_ShowReadExpr :: Expr -> Bool
+prop_ShowReadExpr = undefined
+
+-- | -----------------Part II-----------------------------------------|--
+-- Assigment F
+-------------------------------------------------------------------------
+--
+
