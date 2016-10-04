a381321a7d90498a7b8f5c94aedcb60874d9b9ad
dr-fluff <oliver@Olivers-MacBook-Pro-2.local>
2016-10-04 15:15:18 +0200

    labb 4 part 2

---
+++ Labbar/Labb 4 Drawing Functions /asd.hs
@@ -0,0 +1,17 @@
+
+module Calculator where
+
+import Expr
+
+-- | -----------------Part II-----------------------------------------|--
+-- Assigment F
+-------------------------------------------------------------------------
+--
+points :: Expr -> Double -> (Int,Int) -> [Point]
+points = undefined
+
+-- Assigment G
+-------------------------------------------------------------------------
+--
+readAndDraw :: Elem -> Canvas -> IO ()
+readAndDraw = undefined
